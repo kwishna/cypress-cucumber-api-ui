aa23e4401b8b03789145880cb452ba9794ef86bc
kwishna <kwishna@gmail.com>
2024-07-20 02:21:05 +0530

    tsconfig, eslint added

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type { import("prettier").Config }
+ */
+const config = {
+    semi: false,
+    useTabs: true,
+    tabWidth: 4,
+    quoteProps: 'consistent',
+    printWidth: 160,
+    singleQuote: true,
+    arrowParens: "always",
+    trailingComma: "all",
+    bracketSpacing: true,
+    // arrowParens: true
+};
+
+export default config;
