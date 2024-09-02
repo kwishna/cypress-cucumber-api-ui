27b4b5cfa8a9d075c728d84ea49a53ac488479f4
kwishna <kwishna@gmail.com>
2024-09-03 02:39:43 +0530

    home page loaded

--- cypress/e2e/mocha/travels_app/pages/base_component.ts
+++ cypress/e2e/mocha/travels_app/pages/base_component.ts
@@ -10,6 +10,20 @@ export class BaseComponent {
         cy.log('Navigation to homepage');
         cy.visit('/', { failOnStatusCode: false });
     }
+
+    /**
+     * @description Ensure Home Page is Loaded
+     */
+    ensureHomePageIsLoaded() {
+        cy.get("main div.homepage h4 strong")
+            .should('have.text', 'Your Trip Starts Here!')
+            .parent('h4')
+            .next('p')
+            .should('have.text', 'Let us help you plan your next journey — the one that will leave a lifetime of memories.')
+            .get(this.loader)
+            .should('not.be.visible', { timeout: 10000 });
+    }
+
 }
 
 export const baseComponent = new BaseComponent();
